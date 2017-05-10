54b5e271b9ecc50b74146d340dd264e08b1493f1
Mateusz MikuÅla <mati865@gmail.com>
2017-05-10 13:04:30 +0200

    fix errors

--- app/app.js
+++ app/app.js
@@ -1,23 +1,21 @@
 
-var joke = "JavaScript walked into a bar....';
-var told Joke = "false"
-var $punchline = 'Better watch out for those semi-colons.'
-var result
-var hehesz ek = 'łemata"
-var a=1
-var b='4"
+//var joke = "JavaScript walked into a bar....";
+//var toldJoke = "false";
+//var $punchline = "Better watch out for those semi-colons.";
+//var result;
+var heheszek = "łemata";
+var a=1;
+var b="4";
 
 
-while (a==8) | (b=3)
+while (a===8 || b===3) {
     a++;
-    print(hehesz ek)
-
-<img src="not_existing_foobar.png"
-onerror="alert('Wystąpił błąd')" />
+    console.log(heheszek);
+}
 
 var equal;
 
-if (a != b) {
+if (a !== b) {
     equal = true;
 } else {
     equal = false;
