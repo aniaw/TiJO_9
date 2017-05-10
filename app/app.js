de52c62131eab4541ed5dec0d251de53956bb791
The Almighty <lord@gmail.com>
2017-05-10 10:52:16 +0200

    static-exercise-a

--- app/app.js
+++ app/app.js
@@ -1,31 +1,31 @@
 
 var joke = 'JavaScript walked into a bar....';
-var toldJoke = false;
-var $punchline = 'Better watch out for those semi-colons.';
-//var result;
+var toldJoke = true;
+var $punch_line = 'Better watch out for those semi-colons...';
+var result;
 
 if (toldJoke === true) {
-    console.log($punchline);
+    console.log($punch_line)
 }
 else {
     console.log(joke);
 }
 
-//var zipCode = 98104;
-var joesFavoriteMovie = 'Forbidden Planet';
+var zipCode = 98104;
+var joes_favoriteMovie = Forbidden_Planet;
 var movieTicket$ = 9;
 
 if (movieTicket$ >= 9) {
-    console.log('Too much!');
+    console.log('Too much!')
 } else {
     console.log(' We are going to see ' + joesFavoriteMovie);
 }
 
-var a = 5;
-var b = 56;
-var equal;
+var a = 6
+var b = 6
+var equal
 
-if (a === b) {
+if (a !== b) {
     equal = true;
 } else {
     equal = false;
