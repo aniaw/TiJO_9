f8251638480fc59398a55f7156af9be5da6b8c67
m-gibula <gibula27135@student.pwsztar.edu.pl>
2017-05-10 11:17:06 +0200

    zad2done

--- app/app.js
+++ app/app.js
@@ -1,12 +1,15 @@
 
-var speech = "Well there's this passage I got memorized. Ezekiel 25:17. The path of the righteous man is beset on all sides by the inequities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of the darkness. For he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who attempt to poison and destroy my brothers. And you will know I am the Lord when I lay my vengeance upon you.";
-var toldSpeech = 'false';
-var $result = 'Jules is calm';
-var Vincent;
-var Jules;
-
+var speech = 'Well there\'s this passage I got memorized. ' +
+    'Ezekiel 25:17. The path of the righteous man is beset on all sides by ' +
+    'the inequities of the selfish and the tyranny of evil men. Blessed ' +
+    'is he who, in the name of charity and good will, shepherds the weak ' +
+    'through the valley of the darkness. For he is truly his brother\'s keeper and the finder of lost children. ' +
+    'And I will strike down upon thee with great vengeance and furious anger those who attempt to ' +
+    'poison and destroy my brothers. And you will know I am the Lord when I lay my vengeance upon you.';
+var $punchline ='gałganie';
+var toldJoke = true;
 if (toldJoke === true) {
-    console.log($punchline);
-} else {
     window.console.log(speech);
+} else {
+    window.console.log($punchline);
 }
