fe966cae1b7779e7ea4cb38c0bfa97383588267a
Kamil <sutkowski27185@student.pwsztar.edu.pl>
2017-05-10 10:59:52 +0200

    test

--- app/app.js
+++ app/app.js
@@ -3,20 +3,38 @@ var joke = 'JavaScript walked into a bar....';
 var toldJoke = 'false';
 var $punchline = 'Better watch out for those semi-colons.';
 //var result
+var g=3,j=5,k=6;
+var 'ashdbhasbdkqwbckjqbjcbqjkebcjqejklcbqwjekbcjkwebcjkwbejkvcwbekvbqejkcbqekjfcbqekjbcfjkqebfbqejkbfkjqebfkjqebfjkqebfkjqebfbqekjfbqejkbfjkqebfjkqebjkfbqekjfbjkqebfjkbqejkfkjqebfjkbqejkfbjkqewbfjlwebjvcejlcbjlwebcjlkewbfjlkwebjklcbewjkvbwejkbvjwebvkjwebvkjwebvjkwebvjkwebvjkew';
+
+
+
+
+function myfunction(g,j,k){
+    return (g+j)*k;
+}
+if(g<j){
+    if(k==g){
+        if(j==g){
+            console.log('hahahah');
+        }
+    }
+}
+
 
 if (toldJoke === true) {
     console.log($punchline);
-} else {
+} {
     console.log(joke);
 }
 
 //var zip_code = 98104;
-var joesFavoriteMovie = 'Forbidden Planet';
+var joes_Favorite_Movie = 'Forbidden Planet';
 var movieTicket$ = 9;
 //
 if (movieTicket$ >= 9) {
     console.log('Too much!');
-} else {
+}
+else {
     console.log('We\'re going to see ' + joesFavoriteMovie);
 }
 
@@ -24,7 +42,7 @@ var a = 5;
 var b = '5';
 var equal;
 
-if (a === b) {
+if (a == b) {
     equal = true;
 } else {
     equal = false;
