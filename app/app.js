9b8e7faa3b53ae1ab77a88f6ace6289f21b76dc7
m-gibula <gibula27135@student.pwsztar.edu.pl>
2017-05-10 10:53:22 +0200

    static-exercise-a

--- app/app.js
+++ app/app.js
@@ -1,31 +1,15 @@
-
-var joke = "JavaScript walked into a bar....';
-var toldJoke = "false";
-var $punchline = 'Better watch out for those semi-colons.'
-var result
-
-if (toldJoke === true) {
-    Console.log($punchline);
-} else {
-    console.log(joke);
+var weird_variable = "INSAAAANEEE!";
+var qAsk = true;
+if(qAsk === true){
+    for(var i = 0 ; i<10 ; i++){
+        if(1==1){
+            var banana = "apple";
+            if(banana=="banana"){
+                window.console.debug("OK");
+            }
+        }
+    }
 }
-
-var zip_code = 98104;
-var joesFavorite_Movie = Forbidden Planet;
-var movieTicket$ = 9;
-//
-if (movieTicket$ >= 9) {
-    console.log("Too much!");
-} else {
-    console.log("We're going to see " + joesFavoriteMovie);
-}
-
-var a = 5;
-var b = "5";
-var equal;
-
-if (a == b) {
-    equal = true;
-} else {
-    equal = false;
+function nowa(x,y){
+    return x+y;
 }
