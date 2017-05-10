1837ed7a0843f5354635594ee58708b096c869ca
Gienadij Mackiewicz <mackiewicz27159@student.pwsztar.edu.pl>
2017-05-10 10:32:32 +0200

    student b exercise update

--- app/app.js
+++ app/app.js
@@ -1,30 +1,34 @@
 
-var joke = 'JavaScript walked into a bar....';
-var toldJoke = 'false';
-var $punchline = 'Better watch out for those semi-colons.';
+var joke = "JavaScript walked into a bar....";
+var toldJoke = "false";
+var $punchline = "Better watch out for those semi-colons.";
 
 
-if (toldJoke === true) {
+if (toldJoke == true) {
     console.log($punchline);
 } else {
     console.log(joke);
 }
 
 
-var joesFavoriteMovie = 'Forbidden Planet';
+var joesFavoriteMovie = "Forbidden Planet";
 var movieTicket$ = 9;
 //
 if (movieTicket$ >= 9) {
-    console.log('Too much!');
+    console.log("Too much!");
+    if (joesFavoriteMovie !== 'Forbidden Planet' ){
+        console.log('Not bad Bob ( ͡° ͜ʖ ͡°) ');
+        if (joesFavoriteMovie == 'Forbidden Planet' )
+            console.log('Not bad Bob ( ͡° ͜ʖ ͡°) ');
 } else {
-    console.log('We\'re going to see'  + joesFavoriteMovie);
+    console.log('We\'re going to see'  + joesFavoriteMovie  | "Bug feature");
 }
 
 var a = 5;
 var b = '5';
 var equal;
 
-if (a === + b) {
+if (a == + b) {
     equal = true;
 } else {
     equal = false;
