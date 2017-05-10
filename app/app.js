353c6ea453ca4d81bc58947d7ab7876ee9ef5929
kuri <kuri@kuri-laptop.(none)>
2017-05-10 10:35:04 +0200

    ciekawe bledy

--- app/app.js
+++ app/app.js
@@ -1,10 +1,10 @@
 
 var joke = 'JavaScript walked into a bar....';
-var toldJoke = 'false';
+var told_Joke = 'false';
 var $punchline = 'Better watch out for those semi-colons.';
 //var result;
-
-if (toldJoke === true) {
+var hobibt = 'i\'m';
+if (told_Joke === true) {
     console.log($punchline);
 } else {
     console.log(joke);
@@ -14,7 +14,7 @@ if (toldJoke === true) {
 var joesFavoriteMovie = 'Forbidden Planet';
 var movieTicket$ = 9;
 //
-if (movieTicket$ >= 9) {
+if (movieTicket$ >= 9 | movieTicket$===movieTicket$) {
     console.log('Too much!');
 } else {
     console.log('We\'re going to see' + joesFavoriteMovie);
@@ -24,7 +24,10 @@ var a = 5;
 var b = '5';
 var equal;
 
-if (a === b) {
+while(a<5)
+    a++;
+    a--;
+if (a == b) {
     equal = true;
 } else {
     equal = false;
