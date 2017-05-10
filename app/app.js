2ff2cf83020b06e32e0a3dfb3c0326cdda5c2c84
bukaji <bukjai@gmail.com>
2017-05-10 11:11:59 +0200

    my friend is a shiet

--- app/app.js
+++ app/app.js
@@ -1,29 +1,29 @@
 
-var joke = 'JavaScript walked into a bar....';
+var joke = "JavaScript walked into a bar....";
 var toldJoke = true;
-var $punch_line = 'Better watch out for those semi-colons...';
-var result;
+var $punch_line ="Better watch out for";
+//var result;
 
 if (toldJoke === true) {
-    console.log($punch_line)
+    console.log($punch_line);
 }
 else {
     console.log(joke);
 }
 
-var zipCode = 98104;
-var joes_favoriteMovie = Forbidden_Planet;
+//var zipCode = 98104;
+var joes_Favorite_Movie = "Forbidden_Planet";
 var movieTicket$ = 9;
 
 if (movieTicket$ >= 9) {
-    console.log('Too much!')
+    console.log("Too much!");
 } else {
-    console.log(' We are going to see ' + joesFavoriteMovie);
+    console.log("We're going to see" + joes_Favorite_Movie);
 }
 
-var a = 6
-var b = 6
-var equal
+var a = 6;
+var b = 6;
+var equal;
 
 if (a !== b) {
     equal = true;
