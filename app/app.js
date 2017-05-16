663a32fdd1f6cf6a4c52755e623850d0ed69f03b
duq <ptd.duke@gmail.com>
2017-05-16 23:12:55 +0200

    fixed app.app.js jshint errors

--- app/app.js
+++ app/app.js
@@ -1,17 +1,17 @@
-var string = "dsasdaas
-    sadsd";
+var string = "dsasdaassadsd";
 
-var test = "toooooooo looooonnngggg linneeeeeeeee";
+var test = "iWereTooLong";
 
 console.log(test + string);
 
 var unusedVariable = 10;
+console.log(unusedVariable);//used now
 var x = 1;
 var y = 2;
-if(x < y)
+if(x < y) {
     x += y;
-
-if((x < 10) & (y < 3)) {
+}
+if((x < 10) && (y < 3)) {
     console.log(x + y);
 }
 
