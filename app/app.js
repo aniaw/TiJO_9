de2a7d990452e25679cf55518fa3d637a3c455e7
ReQve <ReQveRe@gmail.com>
2017-05-27 15:54:49 +0200

    LAB9 exercise 2 p2

--- app/app.js
+++ app/app.js
@@ -1,13 +1,13 @@
 
-var a;
+//var a;
 
-var hello_friend = "greetings";
+var helloFriend = 'greetings';
 
-if(hello_friend[0] === 'g') {
-    if(hello_friend[1] === 'r') {
-        if(hello_friend[2] === 'e') {
-            console.log('It\'s probably \"' + hello_friend + '\" or something');
-        }
+if(helloFriend[0] === 'g') {
+    if(helloFriend[1] === 'r') {
+        console.log('It\'s probably \"' +
+                    helloFriend + '\" or something');
+
     }
 }
 
