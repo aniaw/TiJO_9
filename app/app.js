69ec0a266d291fcf171ec04274954d9087f602d1
icamer <maronicam@gmail.com>
2017-05-29 23:33:37 +0200

    half code out cuz of devel: true?

--- app/app.js
+++ app/app.js
@@ -1,24 +1,22 @@
-
-var joke = 'JavaScript walked into a bar....';
-var toldJoke = 'false';
-var $punchline = 'Better watch out for those semi-colons.';
-//var result;
-
-if (toldJoke === true) {
-    console.log($punchline);
-} else {
-    console.log(joke);
-}
+// var joke = 'JavaScript walked into a bar....';
+// var toldJoke = 'false';
+// var $punchline = 'Better watch out for those semi-colons.';
+//
+// if (toldJoke === true) {
+//     console.log($punchline);
+// } else {
+//     console.log(joke);
+// }
 
 //var zip_code = 98104;
-var joesFavoriteMovie = 'Forbidden Planet';
-var movieTicket$ = 9;
+// var joesFavoriteMovie = 'Forbidden Planet';
+// var movieTicket$ = 9;
 //
-if (movieTicket$ >= 9) {
-    console.log('Too much!');
-} else {
-    console.log('We\'re going to see ' + joesFavoriteMovie);
-}
+// if (movieTicket$ >= 9) {
+//     console.log('Too much!');
+// } else {
+//     console.log('We\'re going to see ' + joesFavoriteMovie);
+// }
 
 var a = 5;
 var b = '5';
