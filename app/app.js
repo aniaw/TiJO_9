6eb51f86da73f5f4be38712fa87e8f1cc0ba58df
GKuta95 <g.kuta95@gmail.com>
2017-05-31 08:56:58 +0200

    Bugs fixed in file app.js

--- app/app.js
+++ app/app.js
@@ -1,34 +1,37 @@
 
-var joke = "JavaScript walked into a bar....";
-var toldJoke = "false";
-var $punchline = "Better watch out for those semi-colons.";
+var joke = 'JavaScript walked into a bar....';
+var toldJoke = 'false';
+var $punchline = 'Better watch out for those semi-colons.';
 
 
-if (toldJoke == true) {
+if (toldJoke === true) {
     console.log($punchline);
 } else {
     console.log(joke);
 }
 
 
-var joesFavoriteMovie = "Forbidden Planet";
+var joesFavoriteMovie = 'Forbidden Planet';
 var movieTicket$ = 9;
 //
 if (movieTicket$ >= 9) {
-    console.log("Too much!");
-    if (joesFavoriteMovie !== 'Forbidden Planet' ){
+    console.log('Too much!');
+    if (joesFavoriteMovie !== 'Forbidden Planet') {
         console.log('Not bad Bob ( ͡° ͜ʖ ͡°) ');
-        if (joesFavoriteMovie == 'Forbidden Planet' )
-            console.log('Not bad Bob ( ͡° ͜ʖ ͡°) ');
-} else {
-    console.log('We\'re going to see'  + joesFavoriteMovie  | "Bug feature");
+    }else {
+        console.log('We\'re going to see' + joesFavoriteMovie || 'Bug feature');
+    }
+
+    if (joesFavoriteMovie === 'Forbidden Planet'){
+        console.log('Not bad Bob ( ͡° ͜ʖ ͡°) ');
 }
 
+}
 var a = 5;
 var b = '5';
 var equal;
 
-if (a == + b) {
+if (a===b) {
     equal = true;
 } else {
     equal = false;
